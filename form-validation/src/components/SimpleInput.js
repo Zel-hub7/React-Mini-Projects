6c78fec45a203a773ef2c05fee1c60d2da6709d7
zel-hub7 <zelalemyohannes62@gmail.com>
2023-08-30 06:52:38 -0700

    update files

--- form-validation/src/components/SimpleInput.js
+++ form-validation/src/components/SimpleInput.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const submmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredName.trim() === "") {
+      return;
+    }
+    console.log(enteredName);
+    setEnteredName("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submmitHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
